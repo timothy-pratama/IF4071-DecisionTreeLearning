c6507bb33e5de2d44976fb5ad3d6ce91c4d122a0
Timothy Pratama <timothy.pratama@gmail.com>
2015-09-16 14:46:50 +0700

    Added method read from CSV and ARFF Added data sets

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,6 +1,61 @@
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+import weka.core.converters.ConverterUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by timothy.pratama on 16-Sep-15.
  */
 public class Util {
-    
+
+    public static Instances readARFF(String path)
+    {
+        try
+        {
+            Instances dataSet;
+            ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource(path);
+            dataSet = dataSource.getDataSet();
+            if(dataSet.classIndex() == -1)
+            {
+                dataSet.setClassIndex(dataSet.numAttributes()-1);
+            }
+            return dataSet;
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Instances readCSV(String path)
+    {
+        try
+        {
+            CSVLoader csvLoader = new CSVLoader();
+            csvLoader.setSource(new File(path));
+            Instances dataSet = csvLoader.getDataSet();
+            return dataSet;
+        }
+
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String [] args)
+    {
+        System.out.println("Reading file from ARFF");
+        Instances dataSet = Util.readARFF("data/weather.nominal.arff");
+        System.out.println(dataSet.toString());
+
+        System.out.println("Reading file from CSV");
+        dataSet = Util.readCSV("data/dataSet.csv");
+        System.out.println(dataSet.toString());
+    }
 }
